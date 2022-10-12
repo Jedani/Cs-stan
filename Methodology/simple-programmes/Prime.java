ee8606ddac7db559a5ed444dc69e22b1e210cc22
jedani <allansichangi35@gmail.com>
2022-10-12 07:11:41 +0300

    code for prime number in java

--- Methodology/simple-programmes/Prime.java
+++ Methodology/simple-programmes/Prime.java
@@ -1,15 +1,26 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Prime {
 	public static void main(String args[]) {
 
+		int count = 0;
 		Scanner scn = new Scanner(System.in);
-
 		int x = scn.nextInt();
 
-		// print integer
-		System.out.println("Entered Integer : " + x);
+		for (int i = 2; i < x; i++) {
+			if (x % i == 0) {
+				count++;
+				break;
+			}
+		}
+
+		if (count < 1) {
+			System.out.println(x + " is a prime number");
+		} else {
+			System.out.println(x + " is not a prime number");
+		}
 	}
 }
+
 
 
