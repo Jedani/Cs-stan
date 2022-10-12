e1b7fabe5919bf1cea149de2dbdf3a9487cf20ab
jedani <allansichangi35@gmail.com>
2022-10-12 06:28:10 +0300

    new dir organization

---
+++ Methodology/simple-programmes/Prime.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Prime {
+	public static void main(String args[]) {
+
+		Scanner scn = new Scanner(System.in);
+
+		int x = scn.nextInt();
+
+		// print integer
+		System.out.println("Entered Integer : " + x);
+	}
+}
+
+
